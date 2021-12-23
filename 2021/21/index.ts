ef2f17101dd30a7f457a58f908140a5b5b9f8c2c
Johan Hage <johan.hag3@gmail.com>
2021-12-23 05:56:17 +0100

    Day 21 & 22

--- 2021/21/index.ts
+++ 2021/21/index.ts
@@ -32,7 +32,6 @@ export function one(inputFile: string) {
 	return result
 }
 
-let before: number
 export function two(inputFile: string) {
 	const file = Deno.readTextFileSync(inputFile)
 	const rows = file
@@ -45,8 +44,7 @@ export function two(inputFile: string) {
 
 	const startingPos = rows.map((r) => parseInt(r.split(": ")[1]))
 
-	before = performance.now()
-	findWinner(
+	const winAmount = findWinner(
 		[
 			[startingPos[0], 0],
 			[startingPos[1], 0],
@@ -57,122 +55,37 @@ export function two(inputFile: string) {
 	return Math.max(...winAmount)
 }
 
-const winAmount = [0, 0]
-const earlyReturns = [0, 0]
-let iters = 0
-const maxes: number[] = Array(5).fill(0)
-
 const diceRolls = utils.numArrayCombine([1, 2, 3], utils.numArrayCombine([1, 2, 3], [1, 2, 3])).sort((a, b) => a - b)
-const diceRolls2 = utils.numArrayCombine(diceRolls, diceRolls).sort((a, b) => a - b)
 const m = diceRolls.reduce((prev, d) => ({ ...prev, [d]: (prev[d] ?? 0) + 1 }), {} as Record<number, number>)
-const distribution = Object.values(m)
-distribution.unshift(0)
-distribution.unshift(0)
 
-let iterPerf = 1000000
+const cache: Record<string, [number, number]> = {}
 
-function findWinner(values: [[number, number], [number, number]], playerIndex: number) {
-	iters++
-	if (iters == iterPerf) {
-		iterPerf *= 10
-		console.log(`${iters} iterations took ${performance.now() - before} ms`)
-		console.log(winAmount, earlyReturns, maxes)
-	}
-	const player = values[playerIndex]
-	const other = values[(playerIndex + 1) % 2]
-	if (playerIndex === 0) {
-		const maxt = maxTurns(player[0], player[1])
-		const mint = minTurns(other[0], other[1])
-		if (mint >= maxt) {
-			earlyReturns[0]++
-			maxes[maxt]++
-			winAmount[playerIndex] += 27 ** maxt
-			return
-		}
-	} else {
-		const maxt = maxTurns(other[0], other[1])
-		const mint = minTurns(player[0], player[1])
-		if (mint > maxt) {
-			earlyReturns[1]++
-			maxes[maxt]++
-			winAmount[(playerIndex + 1) % 2] += 27 ** maxt
-			return
-		}
-	}
-	//
-	// let min = player[0] + 9 > 10 ? 1 : player[0] + 3
-	// let max = Math.min(player[0] + 9, 10)
-	// let omin = other[0] + 9 > 10 ? 1 : other[0] + 3
-	// let omax = Math.min(other[0] + 9, 10)
-	// // console.log(getMin(player[0], 2), getMax(player[0], 2), getMin(other[0], 2))
-	// if (player[1] + min >= 21) {
-	// 	winAmount[playerIndex] += 27
-	// 	earlyReturns[0]++
-	// 	return
-	// } else if (player[1] + max < 21 && other[1] + omin >= 21) {
-	// 	earlyReturns[1]++
-	// 	winAmount[(playerIndex + 1) % 2] += 27 ** 2
-	// } else if (other[1] + omax < 21 && player[1] + getMin(player[0], 2) >= 21) {
-	// 	earlyReturns[2]++
-	// 	winAmount[playerIndex] += 27 ** 3
-	// 	return
-	// } else if (player[1] + getMax(player[0], 2) < 21 && other[1] + getMin(other[0], 2) >= 21) {
-	// 	earlyReturns[3]++
-	// 	winAmount[(playerIndex + 1) % 2] += 27 ** 4
-	// 	return
-	// }
+function findWinner(values: [[number, number], [number, number]], turn: number): [number, number] {
+	let player = turn % 2 === 0 ? values[0] : values[1]
+	let other = turn % 2 === 1 ? values[0] : values[1]
+	let cacheId =
+		turn % 2 === 0 ? `[${turn}[${player[0]},${player[1]}],[${other[0]},${other[1]}]]` : `[${turn}[${other[0]},${other[1]}],[${player[0]},${player[1]}]]`
+
+	if (!!cache[cacheId]) return cache[cacheId]
+
+	let wins: [number, number] = [0, 0]
 
 	for (let i = 3; i <= 9; i++) {
 		const p = ((player[0] + i - 1) % 10) + 1
 		if (p + player[1] >= 21) {
-			winAmount[playerIndex] += m[i]
+			wins[turn % 2] += m[i]
 		} else {
-			const newValues: [[number, number], [number, number]] = playerIndex == 0 ? [[p, p + player[1]], values[1]] : [values[0], [p, p + player[1]]]
+			const next: [[number, number], [number, number]] = turn % 2 === 0 ? [[p, p + player[1]], other] : [other, [p, p + player[1]]]
 			for (let j = 0; j < m[i]; j++) {
-				findWinner(newValues, (playerIndex + 1) % 2)
+				const r = findWinner(next, turn + 1)
+				wins = [wins[0] + r[0], wins[1] + r[1]]
 			}
 		}
 	}
-}
 
-function maxTurns(pos: number, s: number) {
-	let turns = 0
-	let score = s
+	cache[cacheId] = [...wins]
 
-	const flip = pos === 1 ? 0 : 1
-	while (score < 21) {
-		turns++
-		score += 4 ** ((flip + turns) % 2)
-	}
-	return turns
-}
-
-function minTurns(p: number, s: number) {
-	if (s > 13 || (s == 11 && p <= 7) || (s == 12 && (p <= 7 || p >= 9)) || (s == 13 && p != 8)) return 1
-	if (s > 1) return 2
-	return 3
-}
-
-function getMax(pos: number, turns: number) {
-	let max = 0
-	for (let i = 1; i <= turns; i++) {
-		if (pos <= 7) {
-			max += 10
-			pos = 10
-		} else {
-			pos--
-			max += pos
-		}
-	}
-	return max
-}
-function getMin(pos: number, turns: number) {
-	const flip = pos === 1 ? 0 : 1
-	let min = 0
-	for (let i = 1; i <= turns; i++) {
-		min += 4 ** ((flip + i) % 2)
-	}
-	return min
+	return wins
 }
 
 export const expectedResult = {
--- 2021/22/index.ts
+++ 2021/22/index.ts
@@ -3,24 +3,205 @@ import * as utils from "../../utils/index.ts"
 export function one(inputFile: string) {
 	let result = 0
 	const file = Deno.readTextFileSync(inputFile)
-	const rows = file
+	const rows: [string, [[number, number], [number, number], [number, number]]][] = file
 		.trim()
 		.split("\n")
 		.map((r) => {
-			const row = r
-			return row
+			const [i, v] = r.split(" ")
+			const [x, y, z] = v.split(",")
+			const [xmin, xmax] = x.split("..")
+			const [ymin, ymax] = y.split("..")
+			const [zmin, zmax] = z.split("..")
+
+			return [
+				i,
+				[
+					[parseInt(xmin.substr(2)), parseInt(xmax)],
+					[parseInt(ymin.substr(2)), parseInt(ymax)],
+					[parseInt(zmin.substr(2)), parseInt(zmax)],
+				],
+			]
 		})
 
-	rows.map((r, index) => {})
+	const xmin = -50
+	const xmax = 50
+
+	const ymin = -50
+	const ymax = 50
+
+	const zmin = -50
+	const zmax = 50
+
+	const reactor = new Set<string>()
+
+	for (const row of rows) {
+		const [xx, yy, zz] = row[1]
+		xx[0] = Math.max(xmin, xx[0])
+		xx[1] = Math.min(xmax, xx[1])
+		yy[0] = Math.max(ymin, yy[0])
+		yy[1] = Math.min(ymax, yy[1])
+		zz[0] = Math.max(zmin, zz[0])
+		zz[1] = Math.min(zmax, zz[1])
+
+		if (row[0] === "on") {
+			for (let x = xx[0]; x <= xx[1]; x++) {
+				for (let y = yy[0]; y <= yy[1]; y++) {
+					for (let z = zz[0]; z <= zz[1]; z++) {
+						reactor.add(`${x},${y},${z}`)
+					}
+				}
+			}
+		} else {
+			for (let x = xx[0]; x <= xx[1]; x++) {
+				for (let y = yy[0]; y <= yy[1]; y++) {
+					for (let z = zz[0]; z <= zz[1]; z++) {
+						reactor.delete(`${x},${y},${z}`)
+					}
+				}
+			}
+		}
+	}
+
+	result = reactor.size
+	return result
+}
+
+type Cube = [[number, number], [number, number], [number, number]]
+
+export function two(inputFile: string) {
+	const file = Deno.readTextFileSync(inputFile)
+	const rows: [string, Cube][] = file
+		.trim()
+		.split("\n")
+		.map((r) => {
+			const [i, v] = r.split(" ")
+			const [x, y, z] = v.split(",")
+			const [xmin, xmax] = x.split("..")
+			const [ymin, ymax] = y.split("..")
+			const [zmin, zmax] = z.split("..")
+
+			return [
+				i,
+				[
+					[parseInt(xmin.substr(2)), parseInt(xmax)],
+					[parseInt(ymin.substr(2)), parseInt(ymax)],
+					[parseInt(zmin.substr(2)), parseInt(zmax)],
+				],
+			]
+		})
+
+	let cubes: Cube[] = []
+
+	for (let i = 0; i < rows.length; i++) {
+		const newCubes: Cube[] = []
+		const [instruction, cube] = rows[i]
+		for (let j = 0; j < cubes.length; j++) {
+			if (cubeContains(cube, cubes[j])) {
+				continue
+			}
+			const intersection = cubeIntersection(cube, cubes[j])
+			if (intersection) {
+				const other = cubes[j]
+				const split = splitCube(other, intersection)
+				split.shift()
+				newCubes.push(...split)
+			} else {
+				newCubes.push(cubes[j])
+			}
+		}
+		if (instruction === "on") {
+			newCubes.push(cube)
+		}
+		cubes = newCubes
+	}
+
+	return cubes.reduce((sum, c) => sum + cubeVolume(c), 0)
+}
+
+function cubeContains(a: Cube, b: Cube) {
+	const [ax, ay, az] = a
+	const [bx, by, bz] = b
+
+	if (bx[0] < ax[0] || bx[1] > ax[1]) return false
+	if (by[0] < ay[0] || by[1] > ay[1]) return false
+	if (bz[0] < az[0] || bz[1] > az[1]) return false
+	return true
+}
+
+function cubeVolume(cube: Cube) {
+	const [x, y, z] = cube
+	const dx = x[1] - x[0] + 1
+	const dy = y[1] - y[0] + 1
+	const dz = z[1] - z[0] + 1
+	return dx * dy * dz
+}
+
+function cubeIntersection(cube1: Cube, cube2: Cube): Cube | undefined {
+	const [c1x, c1y, c1z] = cube1
+	const [c2x, c2y, c2z] = cube2
+
+	// No overlap in any axis
+	if (c1x[1] < c2x[0] || c2x[1] < c1x[0] || c1y[1] < c2y[0] || c2y[1] < c1y[0] || c1z[1] < c2z[0] || c2z[1] < c1z[0]) {
+		return
+	}
+	const [xmin, xmax] = [Math.max(c1x[0], c2x[0]), Math.min(c1x[1], c2x[1])]
+	const [ymin, ymax] = [Math.max(c1y[0], c2y[0]), Math.min(c1y[1], c2y[1])]
+	const [zmin, zmax] = [Math.max(c1z[0], c2z[0]), Math.min(c1z[1], c2z[1])]
+
+	return [
+		[xmin, xmax],
+		[ymin, ymax],
+		[zmin, zmax],
+	]
+}
+
+function splitCube(outer: Cube, inner: Cube) {
+	const [ox, oy, oz] = outer
+	const [ix, iy, iz] = inner
+	const result: Cube[] = []
+
+	result.push([[...ix], [...iy], [...iz]])
+
+	const minX = Math.min(ox[0], ix[0])
+	const maxX = Math.max(ox[1], ix[1])
+	const minY = Math.min(oy[0], iy[0])
+	const maxY = Math.max(oy[1], iy[1])
+	const minZ = Math.min(oz[0], iz[0])
+	const maxZ = Math.max(oz[1], iz[1])
+
+	if (maxX !== ix[1]) {
+		result.push([
+			[ix[1] + 1, maxX],
+			[minY, maxY],
+			[minZ, maxZ],
+		])
+	}
+	if (minX !== ix[0]) {
+		result.push([
+			[minX, ix[0] - 1],
+			[minY, maxY],
+			[minZ, maxZ],
+		])
+	}
+
+	if (maxY !== iy[1]) {
+		result.push([[...ix], [iy[1] + 1, maxY], [minZ, maxZ]])
+	}
+	if (minY !== iy[0]) {
+		result.push([[...ix], [minY, iy[0] - 1], [minZ, maxZ]])
+	}
+
+	if (maxZ !== iz[1]) {
+		result.push([[...ix], [...iy], [iz[1] + 1, maxZ]])
+	}
+	if (minZ !== iz[0]) {
+		result.push([[...ix], [...iy], [minZ, iz[0] - 1]])
+	}
 
 	return result
 }
 
-export function two(inputFile: string) {
-	throw new Error("Not implemented")
-}
-
 export const expectedResult = {
-	debug: [],
-	input: [],
+	debug: [590784, 2758514936282235],
+	input: [580810, 1265621119006734],
 }
