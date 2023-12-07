88c621cb76c4b8e447711e2a7d64b12662d084ef
Johan Hage <johan.hag3@gmail.com>
2023-12-07 08:13:24 +0100

    2023 - Day 5 completed

--- 2023/5/index.ts
+++ 2023/5/index.ts
@@ -61,26 +61,29 @@ export function two(inputFile: string) {
 				for (const row of map) {
 					const low = ranges[j]
 					const high = low + ranges[j + 1]
-					if (low >= row[1] && high <= row[1] + row[2]) {
+					if (low >= row[1] && high < row[1] + row[2]) {
 						ranges[j] = ranges[j] - row[1] + row[0]
 						continue loop1
-					} else if (low >= row[1] && low <= row[1] + row[2]) {
-						console.log(ranges)
-						const newMin = low + row[2]
+					} else if (low >= row[1] && low < row[1] + row[2]) {
+						const newMin = row[1] + row[2]
 						const newRange = high - (row[1] + row[2])
 						ranges[j] = ranges[j] - row[1] + row[0]
-						ranges[j + 1] = ranges[j + 1] - newRange - 1
+						ranges[j + 1] = ranges[j + 1] - newRange
 						ranges.push(newMin, newRange)
-						console.log(ranges)
 						continue loop1
-					} else if (high >= row[1] && high <= row[1] + row[2]) {
-						console.log(ranges)
+					} else if (high >= row[1] && high < row[1] + row[2]) {
 						const newMin = low
 						const newRange = row[1] - low - 1
 						ranges[j] = row[0]
 						ranges[j + 1] = high - row[1]
 						ranges.push(newMin, newRange)
-						console.log(ranges)
+						continue loop1
+					} else if (low <= row[1] && high >= row[1] + row[2]) {
+						ranges[j] = row[0]
+						ranges[j + 1] = row[2]
+
+						ranges.push(low, row[1] - low - 1)
+						ranges.push(row[1] + row[2] + 1, high - (row[1] + row[2] + 1))
 						continue loop1
 					}
 				}
@@ -99,3 +102,5 @@ export const expectedResult = {
 	debug: [35, 46],
 	input: [],
 }
+
+// 153056352
