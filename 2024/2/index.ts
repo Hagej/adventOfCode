7d0f1b5c8500a2b03b9d30f7bc8d391d70575a68
Hage <johan.hag3@gmail.com>
2024-12-19 15:17:06 +0100

    2024 - Day 2 improvements

--- 2024/2/index.ts
+++ 2024/2/index.ts
@@ -1,6 +1,36 @@
 import * as utils from "@utils"
 import fs from "fs"
 
+
+export function both(inputFile: string) {
+	let [p1, p2] = [0, 0]
+	const file = fs.readFileSync(inputFile, "utf-8")
+	const rows = file
+		.trim()
+		.split("\n")
+		.map((r) => {
+			const row = r.split(" ").map((r) => parseInt(r))
+			return row
+		})
+
+	rows.map((r, index) => {
+		if (isSafe(r)) {
+			p1 += 1
+			p2 += 1
+		}
+		else {
+			for (let i = 0; i < r.length - 1; i++) {
+				r.splice(i, 1)
+				if (isSafe(r)) {
+					p2 += 1
+					return
+				}
+			}
+		}
+	})
+
+	return [p1, p2]
+}
 export function one(inputFile: string) {
 	let result = 0
 	const file = fs.readFileSync(inputFile, "utf-8")
@@ -13,8 +43,8 @@ export function one(inputFile: string) {
 		})
 
 	rows.map((r, index) => {
-		if(isSafe(r)) {
-			result += 1	
+		if (isSafe(r)) {
+			result += 1
 		}
 	})
 
@@ -23,22 +53,21 @@ export function one(inputFile: string) {
 
 function isSafe(levels: number[]) {
 	let increasing
-		for(let i = 0; i < levels.length - 1; i++) {
-			const a = levels[i]
-			const b = levels[i + 1]
-			const diff = a - b
-			if(Math.abs(diff) === 0 || Math.abs(diff) > 3) {
-				return false
-			}
-			if(increasing === undefined) {
-				increasing = diff > 0
-			} else if(increasing !== (diff > 0)) {
-				return false
-			}
+	for (let i = 0; i < levels.length - 1; i++) {
+		const a = levels[i]
+		const b = levels[i + 1]
+		const diff = a - b
+		if (Math.abs(diff) === 0 || Math.abs(diff) > 3) {
+			return false
 		}
-		return true
+		if (increasing === undefined) {
+			increasing = diff > 0
+		} else if (increasing !== (diff > 0)) {
+			return false
+		}
+	}
+	return true
 }
-
 export function two(inputFile: string) {
 	let result = 0
 	const file = fs.readFileSync(inputFile, "utf-8")
@@ -51,11 +80,11 @@ export function two(inputFile: string) {
 		})
 
 	rows.map((r, index) => {
-		if(isSafe(r)) {result += 1}
+		if (isSafe(r)) { result += 1 }
 		else {
-			for(let i = 0; i < r.length - 1; i++) {
+			for (let i = 0; i < r.length - 1; i++) {
 				const t = r.splice(i, 1)
-				if(isSafe(r)) {
+				if (isSafe(r)) {
 					result += 1
 					return
 				}
@@ -68,5 +97,5 @@ export function two(inputFile: string) {
 
 export const expectedResult = {
 	debug: [],
-	input: [],
+	input: [483, 528],
 }
