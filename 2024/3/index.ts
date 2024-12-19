32b20c218900d45192e11643f4597171180fec3f
Hage <johan.hag3@gmail.com>
2024-12-19 15:21:18 +0100

    2024 - Day 3 improvements

--- 2024/3/index.ts
+++ 2024/3/index.ts
@@ -1,7 +1,39 @@
 import * as utils from "@utils"
-import { match } from "assert"
 import fs from "fs"
 
+
+export function both(inputFile: string) {
+	let [p1, p2] = [0, 0]
+	const file = fs.readFileSync(inputFile, "utf-8")
+	const rows = file
+		.trim()
+		.split("\n")
+		.map((r) => {
+			const row = r
+			return row
+		})
+
+
+	let t = true
+	rows.map((r, index) => {
+		const matches = [...r.matchAll(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g)]
+
+		for (const match of matches) {
+			if (match[0] === "do()") {
+				t = true
+			} else if (match[0] === "don't()") {
+				t = false
+			} else {
+				const [a, b] = match[0].match(/\d+/g)
+				const product = parseInt(a) * parseInt(b)
+				p1 += product
+				p2 += t ? product : 0
+			}
+		}
+	})
+
+	return [p1, p2]
+}
 export function one(inputFile: string) {
 	let result = 0
 	const file = fs.readFileSync(inputFile, "utf-8")
@@ -65,5 +97,5 @@ export function two(inputFile: string) {
 
 export const expectedResult = {
 	debug: [161, 48],
-	input: [],
+	input: [191183308, 92082041],
 }
