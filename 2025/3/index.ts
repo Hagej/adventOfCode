5f1ad75e363f211b38b9b713ce4593da2a98a3c8
Hage <johan.hag3@gmail.com>
2025-12-04 09:39:55 +0100

    2025 - Day 3

---
+++ 2025/3/index.ts
@@ -0,0 +1,67 @@
+import * as utils from "@utils"
+import fs from "fs"
+
+export function one(inputFile: string) {
+  let result = 0
+  const file = fs.readFileSync(inputFile, "utf-8")
+  const rows = file
+    .trim()
+    .split("\n")
+    .map((r) => {
+      const row = r.split("").map((n) => parseInt(n))
+      return row
+    })
+
+  rows.map((r, index) => {
+    let first = 0
+    let second = 0
+    for (const [index, num] of r.entries()) {
+      if (num > first && index < r.length - 1) {
+        first = num
+        second = 0
+      } else if (num > second) {
+        second = num
+      }
+    }
+    console.log(r, first, second)
+    result += parseInt(`${first}${second}`)
+  })
+
+  return result
+}
+
+export function two(inputFile: string) {
+  let result = 0
+  const file = fs.readFileSync(inputFile, "utf-8")
+  const rows = file
+    .trim()
+    .split("\n")
+    .map((r) => {
+      const row = r.split("").map((n) => parseInt(n))
+      return row
+    })
+
+  rows.map((r, index) => {
+    const digits = Array(12).fill(0)
+    for (const [index, num] of r.entries()) {
+      for (const [i, d] of digits.entries()) {
+        if (num > d && index <= r.length - (12 - i)) {
+          digits[i] = num
+          for (let j = i + 1; j < 12; j++) {
+            digits[j] = 0
+          }
+          break
+        }
+      }
+    }
+    console.log(r, digits)
+    result += parseInt(`${digits.map((d) => `${d}`).join("")}`)
+  })
+
+  return result
+}
+
+export const expectedResult = {
+  debug: [357, 3121910778619],
+  input: [],
+}
